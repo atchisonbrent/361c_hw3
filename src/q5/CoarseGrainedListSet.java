df2844841b50374e17d08f54329cd2eb1c6c2670
Tauseef Aziz <tachief@outlook.com>
2018-10-18 18:08:59 -0500

    fine grained and coarse grained

--- src/q5/CoarseGrainedListSet.java
+++ src/q5/CoarseGrainedListSet.java
@@ -1,68 +1,114 @@
 package q5;
 
-import java.util.Comparator;
-import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class CoarseGrainedListSet implements ListSet {
-  private LinkedList<Integer> list = new LinkedList<>();
-  private ReentrantLock lock = new ReentrantLock();
-	
-  public CoarseGrainedListSet() {
-	// implement your constructor here	  
-  }
-  
-  public boolean add(int value) {
-      lock.lock();
-      Node n = new Node(value);
-      try {
-          if (list.contains(n.value)) return false;
-          return list.add(n.value);
-      } finally {
-          lock.unlock();
-      }
-  }
-  
-  public boolean remove(int value) {
-      lock.lock();
-      Node n = new Node(value);
-      try {
-          return list.remove(n.value);
-      } finally {
-          lock.unlock();
-      }
-  }
-  
-  public boolean contains(int value) {
-      lock.lock();
-      Node n = new Node(value);
-      try {
-          return list.contains(n.value);
-      } finally {
-          lock.unlock();
-      }
-  }
-  
-  protected class Node {
-      public Integer value;
-      public Node next;
-      public Node(Integer x) {
-          value = x;
-          next = null;
-      }
+    // you are free to add members
+    private Node head;
+    private Lock lock = new ReentrantLock();
 
-      @Override
-      public String toString() {
-          return value.toString();
-      }
-  }
 
-  /*
-  return the string of list, if: 1 -> 2 -> 3, then return "1,2,3,"
-  check simpleTest for more info
-  */
-  public String toString() {
-      list.sort(Comparator.comparingInt(Integer::intValue));
-      return list.toString().replaceAll("\\s|\\[|]","") + ",";
-  }
+    public CoarseGrainedListSet() {
+        // implement your constructor here
+        head = new Node(Integer.MIN_VALUE);
+        head.next = new Node(Integer.MAX_VALUE);
+    }
+
+    public boolean add(int value) {
+        // implement your add method here
+        Node previous;
+        Node current;
+        lock.lock();
+        try {
+            previous = head;
+            current = previous.next;
+            while (current.value < value) {
+                previous = current;
+                current = previous.next;
+            }
+            if (value == current.value) {
+                return false;
+            }
+            else {
+                Node node = new Node(value);
+                node.next = current;
+                previous.next = node;
+                return true;
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean remove(int value) {
+        // implement your remove method here
+        Node previous;
+        Node current;
+        lock.lock();
+        try {
+            previous = head;
+            current = previous.next;
+            while (current.value < value) {
+                previous = current;
+                current = previous.next;
+            }
+            if (value == current.value) {
+                previous.next = current.next;
+                return true;
+            } else {
+                return false;
+            }
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    public boolean contains(int value) {
+        // implement your contains method here
+        Node previous;
+        Node current;
+        lock.lock();
+        try {
+            previous = head;
+            current = previous.next;
+            while (current.value < value) {
+                previous = current;
+                current = previous.next;
+            }
+            if (value == current.value) {
+                return true;
+            } else {
+                return false;
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    protected class Node {
+        public Integer value;
+        public Node next;
+
+        public Node(Integer x) {
+            value = x;
+            next = null;
+        }
+    }
+
+    /*
+    return the string of list, if: 1 -> 2 -> 3, then return "1,2,3,"
+    check simpleTest for more info
+    */
+    public String toString() {
+        String result = "";
+        Node previous = head.next;
+        while (previous.next != null) {
+            result = result + previous.value + ",";
+            previous = previous.next;
+        }
+
+        return result;
+    }
 }
--- src/q5/FineGrainedListSet.java
+++ src/q5/FineGrainedListSet.java
@@ -1,41 +1,133 @@
 package q5;
 
-public class FineGrainedListSet implements ListSet {
-// you are free to add members
-	
-  public FineGrainedListSet() {
-    // implement your constructor here	  
-  }
-	  
-  public boolean add(int value) {
-    // implement your add method here	
-    return false;
-  }
-	  
-  public boolean remove(int value) {
-    // implement your remove method here	
-    return false;
-  }
-	  
-  public boolean contains(int value) {
-    // implement your contains method here	
-    return false;
-  }
-	  
-  protected class Node {
-      public Integer value;
-      public Node next;
-      public Node(Integer x) {
-          value = x;
-          next = null;
-      }
-  }
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-  /*
-  return the string of list, if: 1 -> 2 -> 3, then return "1,2,3,"
-  check simpleTest for more info
-  */
-  public String toString() {
-    return "";
-  }
+public class FineGrainedListSet implements ListSet {
+    // you are free to add members
+    private Node head;
+
+    public FineGrainedListSet() {
+        // implement your constructor here
+        head = new Node(Integer.MIN_VALUE);
+        head.next = new Node(Integer.MAX_VALUE);
+
+
+    }
+
+    public boolean add(int value) {
+        // implement your add method here
+        head.lock.lock();
+        Node previous = head;
+        try{
+            Node current = previous.next;
+            current.lock.lock();
+            try{
+                while(current.value < value){
+                    previous.lock.unlock();
+                    previous = current;
+                    current = current.next;
+                    current.lock.lock();
+                }
+                if(current.value == value){
+                    return false;
+                }
+                Node node = new Node(value);
+                node.next = current;
+                previous.next = node;
+                return true;
+            }
+            finally{
+                current.lock.unlock();
+            }
+        }
+        finally{
+            previous.lock.unlock();
+        }
+    }
+
+    public boolean remove(int value) {
+        // implement your remove method here
+        Node previous = null;
+        Node current;
+        head.lock.lock();
+        try{
+           previous = head;
+           current = previous.next;
+           current.lock.lock();
+           try{
+               while(current.value < value){
+                   previous.lock.unlock();
+                   previous = current;
+                   current = current.next;
+                   current.lock.lock();
+               }
+               if(current.value == value){
+                   previous.next = current.next;
+                   return true;
+               }
+               return false;
+           }
+           finally{
+               current.lock.unlock();
+           }
+        }
+        finally{
+            previous.lock.unlock();
+        }
+    }
+
+    public boolean contains(int value) {
+        // implement your contains method here
+        head.lock.lock();
+        Node previous = head;
+        try{
+            Node current = previous.next;
+            current.lock.lock();
+            try{
+                while(current.value < value){
+                    previous.lock.unlock();
+                    previous = current;
+                    current = current.next;
+                    current.lock.lock();
+                }
+                if(current.value == value){
+                    return true;
+                }
+                return false;
+            }
+            finally{
+                current.lock.unlock();
+            }
+        }
+        finally{
+            previous.lock.unlock();
+        }
+    }
+
+    protected class Node {
+        public Integer value;
+        public Node next;
+        public Lock lock = new ReentrantLock();
+
+        public Node(Integer x) {
+            value = x;
+            next = null;
+        }
+    }
+
+    /*
+    return the string of list, if: 1 -> 2 -> 3, then return "1,2,3,"
+    check simpleTest for more info
+    */
+    public String toString() {
+        String result = "";
+        Node previous = head.next;
+        while (previous.next != null) {
+            result = result + previous.value + ",";
+            previous = previous.next;
+        }
+
+        return result;
+    }
 }
